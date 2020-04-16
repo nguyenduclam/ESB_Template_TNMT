2faf7c991849d1873869bbc858905676725d2a2a
Nguyen Duc Lam <nguyenduclam0605@gmail.com>
2020-04-17 01:17:24 +0700

    ESB_template_TNMT

---
+++ README.md
@@ -0,0 +1,8 @@
+# ESB Template Tài nguyên - Môi trường
+
+# References
++ Custom Input Files: https://codepen.io/sazzad/pen/antDJ
++ Hide Scrollbar but still 
+being able to Scroll: https://www.geeksforgeeks.org/hide-scroll-bar-but-while-still-being-able-to-scroll-using-css/
++ Data Tables: https://datatables.net/
++ Cổng dữ liệu TPHCM: https://data.hochiminhcity.gov.vn/
---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+/*---- Modal Login ----*/
+$("#login-btn").click(function () {
+    $("#loginModal").modal("show");
+    return false;
+});
+
+/*---- Register Login ----*/
+$("#register-btn").click(function () {
+    $("#registerModal").modal("show");
+    return false;
+});
+
+/*---- Input Files Customs ----*/
+$('#chooseFile').bind('change', function () {
+    var filename = $("#chooseFile").val();
+    if (/^\s*$/.test(filename)) {
+        $(".file-upload").removeClass('active');
+        $("#noFile").text("Chọn file...");
+    } else {
+        $(".file-upload").addClass('active');
+        $("#noFile").text(filename.replace("C:\\fakepath\\", ""));
+    }
+});
+
+/*---- Register Form ----*/
+$("#organ_pers").change(function () {
+    $("#organ_pers option:selected").each(function () {
+        if ($("#organ_pers option:selected").text() == "Cá nhân") {
+            $("#personal").show(250);
+            $("#organization_type").hide(250);
+        } else {
+            $("#organization_type").show(250);
+            $("#personal").hide(250);
+        }
+    });
+}).trigger("change");
+
+/*---- Draggable Modal ----*/
+$(".modal-header").on("mousedown", function (mousedownEvt) {
+    var $draggable = $(this);
+    var x = mousedownEvt.pageX - $draggable.offset().left,
+        y = mousedownEvt.pageY - $draggable.offset().top;
+    $("body").on("mousemove.draggable", function (mousemoveEvt) {
+        $draggable.closest(".modal-dialog").offset({
+            "left": mousemoveEvt.pageX - x,
+            "top": mousemoveEvt.pageY - y
+        });
+    });
+    $("body").one("mouseup", function () {
+        $("body").off("mousemove.draggable");
+    });
+    $draggable.closest(".modal").one("bs.modal.hide", function () {
+        $("body").off("mousemove.draggable");
+    });
+});
+
+/*---- Datables ----*/
+$(document).ready(function () {
+    $('#table_history').dataTable({
+        paging: true,
+        autoWidth: false,
+        "language": {
+            pagingType: "full_numbers",
+            search: '<span>Tìm kiếm:</span> _INPUT_',
+            searchPlaceholder: 'Gõ để tìm...',
+            paginate: {
+                'first': 'First',
+                'last': 'Last',
+                'next': $('html').attr('dir') == 'rtl' ? '<span style="font-size:20px;">Trước</span>' :
+                    '<span style="font-size:20px;">Sau</span>',
+                'previous': $('html').attr('dir') == 'rtl' ? '<span style="font-size:20px;">Sau</span>' :
+                    '<span style="font-size:20px;">Trước</span>'
+            },
+            sLengthMenu: "<span>Hiển thị</span> _MENU_ <span>dòng trên 1 bảng</span>",
+            sZeroRecords: "Không tìm thấy kết quả",
+            sInfo: "Hiển thị _START_ đến _END_ trên _TOTAL_ dòng",
+            sInfoFiltered: "(tất cả _MAX_ dòng)",
+            sInfoEmpty: "Hiển thị 0 đến _END_ trên _TOTAL_ dòng"
+        }
+    });
+});
